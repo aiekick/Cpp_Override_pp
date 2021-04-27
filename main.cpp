57eb98dce5ee56b879805fda200aae4a6b7f99e1
Aiekick <aiekick@funparadigm.com>
2021-04-27 23:33:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,86 +49,18 @@ std::ostream& operator<< (std::ostream& stm, Num v) {stm << v.m_Value; return st
 
 int main ()
 {
-	// https://stackoverflow.com/questions/949433/why-are-these-constructs-using-pre-and-post-increment-undefined-behavior#30914692
-	
 	{
 		int i = 5;
 		i = i++ + ((++i) * (i--));
-		// ordre :
-		// i-- => 5
-		// ++i => 6
-		// 5*6 => 30
-		// i++ => 6
-		// 30 + 6 => 36
-		// 36 +1 - 1 => 36
-		// msvc : retour 42 !?
-		// mingw32 : retour 47 !?
-		// clang : retour 54 !?
 		std::cout << "int i(5), i = i++ + ++i * i-- => " << i << std::endl;
 	}
 
 	{
+		std::cout << "Num i(5), i = i++ + ++i * i-- => " << std::endl;
 		Num i = 5;
 		i = i++ + ((++i) * (i--));
-		// ordre :
-		// i-- => 5
-		// ++i => 6
-		// 5*6 => 30
-		// i++ => 6
-		// 30 + 6 => 36
-		// 36 +1 - 1 => 36
-		// msvc : retour 30 !?
-		// mingw32 : retour 30 !?
-		// clang : retour 54 !?
-		std::cout << "Num i(5), i = i++ + ++i * i-- => " << i << std::endl;
+		std::cout << "result => " << i << std::endl;
 	}
 
-	// clang sur win10 donne ca : "via cmake"
-	// int i(5), i = i++ + ++i * i-- => 54
-	// i++ : 6
-	// ++i : 7
-	// i-- : 6
-	// 7 * 7 : 49
-	// 5 + 49 : 54
-	// Num i(5), i = i++ + ++i * i-- => 54
-	
-	// Mingw32 sur win10 donne ca : "via cmake"
-	// int i(5), i = i++ + ++i * i-- => 47
-	// i-- : 4
-	// ++i : 5
-	// 5 * 5 : 25
-	// i++ : 6
-	// 5 + 25 : 30
-	// Num i(5), i = i++ + ++i * i-- => 30
-	
-	// MSVC sur win10 donne ca : "via cmake"
-	// int i(5), i = i++ + ++i * i-- => 42
-	// i-- : 4
-	// ++i : 5
-	// 5 * 5 : 25
-	// i++ : 6
-	// 5 + 25 : 30
-	// Num i(5), i = i++ + ++i * i-- => 30
-	
-	// g++ sur linux donne ca : "g++ main.cpp -o main -v"
-	// int i(5), i = i++ + ++i * i-- => 47
-	// i-- : 4
-	// ++i : 5
-	// 5 * 5 : 25
-	// i++ : 6
-	// 5 + 25 : 30
-	// Num i(5), i = i++ + ++i * i-- => 30
-	
-	// clang sur linux donne ca : "clang++ -std=c++11 -pedantic -Wall main.cpp -o main -v"
-	// int i(5), i = i++ + ++i * i-- => 54
-	// i++ : 6
-	// ++i : 7
-	// i-- : 6
-	// 7 * 7 : 49
-	// 5 + 49 : 54
-	// Num i(5), i = i++ + ++i * i-- => 54
-	
-	// conlusion : mieux vaut eviter les ++ ou -- en post dans des expression composés
-	
 	return 0;
 }
